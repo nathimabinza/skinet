586e45e7d4f467866366bac0b4da53e166548f23
Nathi Mabinza <mabinza.nathi@gmail.com>
2023-02-23 16:23:27 +0200

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddDbContext<StoreContext>(opt => {
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,6 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -1,17 +1,11 @@
 ﻿using Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Core.Interfaces
 {
     public  interface IProductRepository
     {
       Task<Product> GetProductByIdAsync(int id);
-        Task<IReadOnlyList<Product>> GetProducts();
-        Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync();
-        Task<IReadOnlyList<ProductType>> GetProductTypeAsync();
+      Task<IReadOnlyList<Product>> GetProducts();
+      Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync();
+      Task<IReadOnlyList<ProductType>> GetProductTypeAsync();
     }
 }
